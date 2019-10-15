e0fadc926a6ea02aa0909f3b8a16d1beef4f97c6
gf <vafaei_ar@yahoo.com>
2019-10-15 12:59:59 +0330

    day8

--- Day8/test1.py
+++ Day8/test1.py
@@ -1,9 +1,5 @@
 class Test:
-    def __new__(self,*args,**kwargs):
-        print('the new method is called.')
-        x = super().__new__(self)
-        return x
-        
+       
     def __init__(self,x):
         print('the init method is called.')
         print(x,'is taken as argument')
